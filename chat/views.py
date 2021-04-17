3ed3ceed992ab2510097f1846d087d467e0ef346
Silvio Molina <molinahuel44@gmail.com>
2021-04-16 23:39:15 -0300

    starting with a chatroom

---
+++ chat/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+	context = {
+		'somecontext':'Hellow'
+	}
+	return render(request, 'index.html', context) 
+
+def room(request, room_name):
+	context = {'room_name':room_name}
+	return render(request, 'chatroom.html', context)
