4497be49c83310d6e82701254482dce16947aa48
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-30 00:32:51 -0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -8,3 +8,21 @@ class Event(models.Model):
 	date_to_launch	= models.IntegerField()
 	description		= models.TextField(max_length=60)
 
+class Post(models.Model):
+
+	name			= models.CharField(max_length=30)
+	user_owner	= models.ForeignField(User)
+	date_created	= models.IntegerField()
+	date_to_launch	= models.IntegerField()
+	description		= models.TextField(max_length=60)
+        likes           = models.IntegerField()
+        time_shared     = models.IntegerField()
+        comments        = models.OneToManyField()
+
+class Comment(models.Model):
+
+	name			= models.CharField(max_length=30)
+	user_asociated	= models.ForeignField(User)
+	date_created	= models.IntegerField()
+	date_to_launch	= models.IntegerField()
+	description		= models.TextField(max_length=60)
