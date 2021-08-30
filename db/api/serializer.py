557c265c612488a3a054518e4e6f0c34a4676d10
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-30 01:10:28 -0300

    Update serializer.py

--- db/api/serializer.py
+++ db/api/serializer.py
@@ -2,6 +2,13 @@ from db.models import Cat, Dog, Comment, Post, Event
 from rest_framework import serializers
 
 class EventSerializer(serializers.ModelSerializer):
-	class Meta:
+	
+        def more_than_18(value):
+              if value < 18:
+                  raise serializers.ValidationError('Not sufficient old, you have to have more than 18 years old')
+
+        age_owner = IntegerField(validators=[self.more_than_18])
+
+        class Meta:
 		fields	= '__all__'
 		model 	= Cat 
