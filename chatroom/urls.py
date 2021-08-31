5a53e33af512c84ba1a61b515189ccdf19f81a91
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-31 18:57:24 -0300

    Update urls.py

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_swagger.views import get_swagger_view
+
 from accounts.api.views import LoginView
 
+schema_view = get_swagger_view(title='API name')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
+    path('admin/',  admin.site.urls),
+    path('chat/',   include('chat.urls')),
     path('player/', include(('accounts.api.urls', 'api_name'), namespace='instance_name')),
-    path('prove/', LoginView.as_view())
+    path('prove/',  LoginView.as_view()),
+    path('api_view/', schema_view)
 ]
