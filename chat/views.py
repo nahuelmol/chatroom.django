3f54e6e4d0c5fdaf3c13d9c27c5c03459d500b25
Silvio Molina <molinahuel44@gmail.com>
2021-09-23 00:54:00 -0300

    modification

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
-from db.models import Chatroom
+from db.models import Chatroom, Message
 from db.views import create_chatroom
 
 @login_required(login_url='/accounts/login/')
@@ -15,14 +15,24 @@ def index(request):
 	return render(request, 'index.html', context) 
 
 
+
+
 def room(request, room_name):
-	context = {	'room__name':room_name}
 	array = []
+	msg_array = []
 
 	for each in Chatroom.objects.all():
 		array.append(each.chatroom_name)
 
 	if not (room_name in array):
 		create_chatroom(room_name, request.user)
+
+	for each in Message.objects.all():
+		if each.chatroom == 'chat_'+ room_name:
+			msg_array.append(each)
+
+	context = {	'room__name':room_name,
+				'messages':msg_array
+		}
 
 	return render(request, 'chatroom.html', context)
