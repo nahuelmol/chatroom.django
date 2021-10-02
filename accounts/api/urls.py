233f285e0241864fa64ff54362dc6db1dad7704c
Silvio Molina <molinahuel44@gmail.com>
2021-10-02 10:24:12 -0300

    adding login, logout and register

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,21 +1,21 @@
 from rest_framework import routers
 
-from accounts.api.views import LoginView, UserViewSet
+from accounts.api.views import LoginView, UserViewSet, UserLogout, RegisterView
 from django.urls import include, path
 
 from rest_framework.authentication import TokenAuthentication
 
 app_name = 'api_name'
-#router = routers.SimpleRouter(trailing_slash=False) 
 router = routers.SimpleRouter()
 
 login_view		= LoginView.as_view()
-
-router.register('users', UserViewSet)
-#router.register(r'login/', 	login_view,	basename='login')
+logout_view 	= UserLogout.as_view()
+register_view 	= RegisterView.as_view()
 
 urlpatterns = [
-	path('login/',	login_view),
+	path('login/',		login_view),
+	path('logout/',		logout_view),
+	path('register/',	register_view)
 ]
 
 urlpatterns += router.urls
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -9,10 +9,16 @@ from rest_framework.decorators import action
 from rest_framework import viewsets, status
 
 from accounts.api.serializers import UserSerializer
+from accounts.utils import generate_access_token
 
 from django.contrib.auth.models import User
 
 class LoginView(APIView):
+	authentication_classes = [
+		authentication.TokenAuthentication]
+
+	permission_classes = [
+		permissions.AllowAny]
 
 	@classmethod
 	def get_extra_actions(cls):
@@ -22,11 +28,81 @@ class LoginView(APIView):
 
 		u_name 	= request.data.get('username')
 		p_word 	= request.data.get('password')
+		headers = request.META
+
 		user 	= authenticate(username=u_name, password=p_word)
 		if user:
-			return Response({'token':user.auth_token.key})
+			messages.success(request, 'logged in')
+			user_access_token = generate_access_token(user)
+
+			if user_access_token:
+				response = redirect(headers.get('HTTP_REFERER', '/'))
+				response.set_cookie(key='access_token', value=user_access_token,httponly=True)
+				return response
+
+			response = redirect(headers.get('HTTP_REFERER'),'/')
+			messages.add_message(request, messages.INFO, 'cannot be created a token for the user')
+			return response
 		else:
-			return Response({'error':'wrong credentials'}, status=status.HTTP_400_BAD_REQUEST)
+			messages.error(request, 'not a user to log in')
+			response = redirect(headers.get('HTTP_REFERER', '/'),status=status.HTTP_400_BAD_REQUEST)
+			return response
+
+class RegisterView(APIView):
+	authentication_classes = [
+		authentication.TokenAuthentication]
+	permission_classes = [
+		permissions.AllowAny]
+
+	def post(self, request):
+		headers 	= request.META
+		email_ 		= request.data.get('email')
+		username_ 	= request.data.get('username')
+		pass_ 		= request.data.get('password')
+
+		new_user = User(
+				email=email_,
+				username=username_
+				)
+		new_user.set_password(pass_)
+		new_user.save()
+
+		if new_user:
+			access_token = generate_access_token(new_user)
+			if access_token:
+				response = redirect(headers.get('HTTP_REFERER', '/'))
+				response.set_cookie(key='access_token', value=access_token)
+				response.data = {'access_token': 'created'}
+		
+				return response
+			else:
+				response = redirect(headers.get('HTTP_REFERER', '/'))
+				response.data = {'access_token':'was not created'}
+				return response
+		else:
+			response = redirect(headers.get('HTTP_REFERER', '/'))
+			response.data = {'user':'there is no such user'}
+			return response
+
+class UserLogout(APIView):
+	authentication_classes 	= [
+		authentication.TokenAuthentication]
+	permission_classes 		= [
+		permissions.AllowAny]
+
+	def get(self, request):
+
+		user_token  = request.COOKIES.get('access_token', None)
+		headers 	= request.META
+
+		if user_token:
+			response = redirect(headers.get('HTTP_REFERER', '/login'))
+			response.delete_cookie('access_token')
+			messages.success(request, 'logged out successfully')
+			return response 
+		response = redirect(headers.get('HTTP_REFERER', '/homepage'))
+		messages.success(request, 'you are already logged out')
+		return response
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
